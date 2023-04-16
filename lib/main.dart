e25b83bcbcbc7a217be83cf5859f85b3849b9cad
clean17 <piw940317@gmail.com>
2023-04-16 22:37:05 +0900

    http get 요청 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+<<<<<<< HEAD
 import 'package:village/view/pages/host/host_place_resister_page.dart';
+=======
+import 'package:village/view/pages/test/json_parse.dart';
+>>>>>>> ef24334 (http get 요청 확인)
 
 void main() {
   runApp(
@@ -20,7 +24,11 @@ class MyApp extends StatelessWidget {
           // primaryColor: Colors.white,
           ),
       debugShowCheckedModeBanner: false, // 화면에 디버그 제거
+<<<<<<< HEAD
       home: const HostPlaceResisterPage(), // 클래스 분리
+=======
+      home: const JsonParse(), // 클래스 분리
+>>>>>>> ef24334 (http get 요청 확인)
       // home: Scaffold(
       //   body: Container(
       //     child: const PlaceDetailPage(),
