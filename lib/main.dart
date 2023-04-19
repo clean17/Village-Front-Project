9811c03eb588a13cba4eb527ed113eca16fa2f5f
clean17 <piw940317@gmail.com>
2023-04-19 18:24:42 +0900

    주변 지도 수정

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:village/core/constants/move.dart';
 import 'package:village/model/user/user_repository.dart';
 import 'package:village/provider/session_provider.dart';
+import 'package:village/view/pages/map/map_nearby_place_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:village/view/pages/test/my_home_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -79,50 +83,4 @@ class MyApp extends ConsumerWidget {
 // // 안드로이드 에뮬레이터(가상 디바이스)에서는 localhost(127.0.0.1)을 사용할 수 없음
 // // 10.0.2.2는 안드로이드 에뮬레이터에서 호스트 컴퓨터를 가리키는 가상 IP 주소임
 // String serverUrl = "http://10.0.2.2:8080/fcm/token"; // 서버 주소
-
-// class _HomePageState extends State<HomePage> {
-//   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     // FCM 토큰 가져오기
-//     _firebaseMessaging.getToken().then((token) {
-//       print('FCM Token: $token');
-
-//       sendTokenToServer(token!);
-//     });
-
-//     // FCM 메시지 수신 이벤트 등록
-//     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-//       print(message);
-//       print("FCM Message Received: ${message.notification?.body}");
-//     });
-//   }
-
-//   // APP을 실행하였을 때 FCM 토큰을 Server로 보내기 위한 메서드
-//   void sendTokenToServer(String token) async {
-//     try {
-//       final response = await http.post(
-//         Uri.parse(serverUrl),
-//         body: {
-//           "token": token,
-//         },
-//       );
-//       if (response.statusCode == 200) {
-//         print("Token sent successfully");
-//       } else {
-//         print("Failed to send token");
-//       }
-//     } catch (e) {
-//       print("Error sending token: $e");
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold();
-//   }
-// }
-
+  
