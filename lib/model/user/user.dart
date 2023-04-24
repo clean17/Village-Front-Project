b4fecd43cc92ad67f070082a6af00cb28b6eb6e8
LeeInHwa <2inhwa5@gmail.com>
2023-04-24 19:30:52 +0900

    usercontroller, repository 추 추가 및 수정

--- lib/model/user/user.dart
+++ lib/model/user/user.dart
@@ -1,14 +1,14 @@
 class User {
   int? id;
-  String? username;
+  String? name;
   String? email;
 
-  User({this.id, this.username, this.email});
+  User({this.id, this.name, this.email});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json["id"],
-      username: json["username"],
+      name: json["name"],
       email: json["email"],
     );
   }
@@ -16,7 +16,7 @@ class User {
   Map<String, dynamic> toJson() {
     return {
       "id": id,
-      "username": username,
+      "name": name,
       "email": email,
     };
   }
--- lib/view/pages/user/info_update_page/widgets/profile_pic.dart
+++ lib/view/pages/user/info_update_page/widgets/profile_pic.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class ProfilePic extends StatelessWidget {
+class ProfilePic extends StatefulWidget {
   const ProfilePic({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePic> createState() => _ProfilePicState();
+}
+
+class _ProfilePicState extends State<ProfilePic> {
+
 
   @override
   Widget build(BuildContext context) {
