6c120fa413c10343dcdf08070e119dc6f01de8b2
clean17 <piw940317@gmail.com>
2023-04-26 14:38:31 +0900

    등록 페이지 수정

--- lib/view/pages/host/resister_place_page/widgets/host_resister_body.dart
+++ lib/view/pages/host/resister_place_page/widgets/host_resister_body.dart
@@ -22,48 +22,53 @@ class HostResisterBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const ImageInputBox(),
-            const CommonFormField(hintText: '공간명을 입력하세요', prefixText: '제목'),
-            const JusoSearchPage(),
-            const CommonFormField(hintText: '상세주소를 입력하세요', prefixText: '상세주소'),
-            const CommonFormField(
-                hintText: '소개 내용을 입력하세요', prefixText: '장소 소개'),
-            const CommonFormField(
-                hintText: '유의 사항을 입력하세요', prefixText: '유의 사항'),
-            const TelFormField(hintText: '010-1234-5678', prefixText: '전화번호'),
-            const HashtagFormField(hintText: '해시태그를 입력하세요', prefixText: '해시태그'),
-            const NumFormField(
-                hintText: '시간당 가격을 입력하세요', prefixText: '시간당  가격'),
-            const FacilityTable(),
-            const CategoryTable(),
-            const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '요일선택',
-                  style: mblack_text,
-                )),
-            const SizedBox(
-              height: 10,
-            ),
-            const DateSelect(),
-            Row(
-              children: const [
-                PlaceTimePicker(text: '예약 시작 시간'),
-                PlaceTimePicker(text: '예약 종료 시간'),
-              ],
-            ),
-            Row(
-              children: const [
-                PeoplePicker(text: '최대 예약 인원'),
-                ParkingPicker(),
-              ],
-            ),
-          ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const ImageInputBox(),
+              const CommonFormField(hintText: '공간명을 입력하세요', prefixText: '제목'),
+              const JusoSearchPage(),
+              const CommonFormField(
+                  hintText: '상세주소를 입력하세요', prefixText: '상세주소'),
+              const CommonFormField(
+                  hintText: '소개 내용을 입력하세요', prefixText: '장소 소개'),
+              const CommonFormField(
+                  hintText: '유의 사항을 입력하세요', prefixText: '유의 사항'),
+              const TelFormField(hintText: '010-1234-5678', prefixText: '전화번호'),
+              const HashtagFormField(
+                  hintText: '해시태그를 입력하세요', prefixText: '해시태그'),
+              const NumFormField(
+                  hintText: '시간당 가격을 입력하세요', prefixText: '시간당  가격'),
+              const FacilityTable(),
+              const CategoryTable(),
+              const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '요일선택',
+                    style: mblack_text,
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              const DateSelect(),
+              Row(
+                children: const [
+                  PlaceTimePicker(text: '예약 시작 시간'),
+                  PlaceTimePicker(text: '예약 종료 시간'),
+                ],
+              ),
+              Row(
+                children: const [
+                  PeoplePicker(),
+                  ParkingPicker(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/pages/user/login_page/login_page.dart
+++ lib/view/pages/user/login_page/login_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:village/core/constants/color.dart';
-import 'package:village/view/pages/user/join_page/join_page.dart';
 import 'package:village/view/pages/user/login_page/widgets/login_body.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.black),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(color: Colors.black),
+        ),
+        body: const LoginBody(),
       ),
-      body: LoginBody(),
     );
   }
 }
