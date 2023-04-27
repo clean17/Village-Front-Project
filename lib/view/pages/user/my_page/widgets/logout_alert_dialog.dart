2c561106115f0558ebcef84b0b00ea6cfb28bcf5
LeeInHwa <2inhwa5@gmail.com>
2023-04-27 09:49:31 +0900

    logout 구현 완료

--- lib/view/pages/user/my_page/widgets/logout_alert_dialog.dart
+++ lib/view/pages/user/my_page/widgets/logout_alert_dialog.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:village/controller/user_controller.dart';
 
-class LogoutAlertDialog extends StatelessWidget {
+class LogoutAlertDialog extends ConsumerWidget {
   const LogoutAlertDialog({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AlertDialog(
       // title: Text("로그아웃"),
       content: Text("로그아웃을 하시겠습니까?"),
       actions: <Widget>[
         TextButton(
-          onPressed: () => Navigator.pop(context, 'OK'),
+          onPressed: () => ref.read(userControllerProvider).logout(),
           child: const Text('확인'),
         ),
         TextButton(
