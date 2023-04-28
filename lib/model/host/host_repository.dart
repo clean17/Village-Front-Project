092e8b85c4eaf2f0fb0724f8b34c11f48168aa5b
LeeInHwa <2inhwa5@gmail.com>
2023-04-28 19:30:04 +0900

    host model 추가

---
+++ lib/model/host/host_repository.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+import 'package:village/core/constants/http.dart';
+import 'package:village/dto/host_request.dart';
+import 'package:village/dto/response_dto.dart';
+import 'package:village/dto/user_request.dart';
+import 'package:village/model/host/host.dart';
+import 'package:village/model/user/user.dart';
+
+
+class HostRepository {
+  static final HostRepository _instance = HostRepository._single();
+  factory HostRepository() {
+    return _instance;
+  }
+  HostRepository._single();
+
+
+  Future<ResponseDTO> fetchHostJoin(HostReqDto hostReqDto) async {
+    try {
+      Response response = await dio.post("/host", data: hostReqDto.toJson());
+      Logger().d("${response.data}");
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      Logger().d("2");
+      responseDTO.data = Host.fromJson(responseDTO.data);
+      return responseDTO;
+    } catch (e) {
+      return ResponseDTO(code: -1, msg: "호스트 신청 실패");
+    }
+  }
+}
