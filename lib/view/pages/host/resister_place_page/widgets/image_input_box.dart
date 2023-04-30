16ec1d7258c9933d62129d41354a77ee8e9e8cc4
clean17 <piw940317@gmail.com>
2023-04-30 15:34:55 +0900

    해시태그 리스트 프로바이더 저장

--- lib/view/pages/host/resister_place_page/widgets/image_input_box.dart
+++ lib/view/pages/host/resister_place_page/widgets/image_input_box.dart
@@ -9,15 +9,15 @@ import 'package:village/view/pages/host/resister_place_page/host_resister_place_
 class ImageInputBox extends ConsumerWidget {
   ImageInputBox({
     super.key,
+    required this.vm,
+    this.files,
   });
 
-  List<File>? files = [];
+  HostResisterPlacePageViewModel vm;
+  List<File>? files;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    HostResisterPlacePageViewModel vm =
-        ref.read(hostResisterPlacePageProvider.notifier);
-    HostResisterPlacePageModel? pm = ref.read(hostResisterPlacePageProvider);
     return Column(
       children: [
         Container(
@@ -26,60 +26,65 @@ class ImageInputBox extends ConsumerWidget {
           height: 200,
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
           // 이미지 추가 시 화면에 보여줌
-          child: pm?.files?.isEmpty == true
+          child: files!.isEmpty == true
               ? const Center(
                   child: Text(
                   '공간 이미지를 추가하세요',
                   style: mlarge_black_text,
                 ))
-              : Expanded(
-                  child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: List.generate(
-                        pm!.files!.length,
-                        (index) =>
-                            // 이미지 리스트
-                            Stack(
-                          children: [
-                            Card(
-                              child: Image.file(
-                                height: 200,
-                                pm.files![index],
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                                right: 0,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black
-                                            .withOpacity(0.6), // 그림자 색상
-                                        spreadRadius:
-                                            -8, // 그림자 전체 크기를 얼마나 확장시킬 것인지
-                                        blurRadius: 8, // 그림자의 흐림 정도를 설정합니다.
-                                        offset: const Offset(
-                                            0, 0), // 그림자 위치 조정을 위한 오프셋 값
+              : Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    Expanded(
+                      child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: List.generate(
+                            files!.length,
+                            (index) =>
+                                // 이미지 리스트
+                                Stack(
+                              children: [
+                                Card(
+                                  child: Image.file(
+                                    height: 200,
+                                    files![index],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                    right: 0,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.black
+                                                .withOpacity(0.6), // 그림자 색상
+                                            spreadRadius:
+                                                -8, // 그림자 전체 크기를 얼마나 확장시킬 것인지
+                                            blurRadius: 8, // 그림자의 흐림 정도를 설정합니다.
+                                            offset: const Offset(
+                                                0, 0), // 그림자 위치 조정을 위한 오프셋 값
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  ),
-                                  child: IconButton(
-                                    color: Colors.white,
-                                    icon: const Icon(
-                                      Icons.close,
-                                      size: 30,
-                                    ),
-                                    onPressed: () {
-                                      files!.removeAt(index);
-                                      vm.notifyChangeImages(files!);
-                                    },
-                                  ),
-                                ))
-                          ],
-                        ),
-                      )),
+                                      child: IconButton(
+                                        color: Colors.white,
+                                        icon: const Icon(
+                                          Icons.close,
+                                          size: 30,
+                                        ),
+                                        onPressed: () {
+                                          files!.removeAt(index);
+                                          vm.notifyChangeImages(files!);
+                                        },
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          )),
+                    )
+                  ],
                 ),
         ),
         Padding(
@@ -92,9 +97,11 @@ class ImageInputBox extends ConsumerWidget {
                   maxWidth: 200,
                 );
                 if (xfiles.isNotEmpty) {
-                  files?.addAll(
-                      xfiles.map((image) => File(image.path)).toList());
+                  files!
+                      .addAll(xfiles.map((image) => File(image.path)).toList());
+                  // 상태는 한번에 하나의 상태만 변경 가능
                   vm.notifyChangeImages(files!);
+                  vm.notifyChangeFile(files!);
                 }
               },
               child: const Text('이미지 추가')),
