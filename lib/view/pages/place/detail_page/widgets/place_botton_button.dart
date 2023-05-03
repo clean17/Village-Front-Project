43e3ad0aee879d8320edf60c5a1da137c5d16994
clean17 <piw940317@gmail.com>
2023-05-03 15:34:29 +0900

    공간 예약 정보 입력 프로바이더 연결

--- lib/view/pages/place/detail_page/widgets/place_botton_button.dart
+++ lib/view/pages/place/detail_page/widgets/place_botton_button.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:village/core/constants/move.dart';
 import 'package:village/core/constants/size.dart';
 import 'package:village/core/constants/style.dart';
+import 'package:village/provider/picker_provier.dart';
+import 'package:village/view/pages/host/resister_place_page/widgets/people_picker.dart';
+import 'package:village/view/pages/host/resister_place_page/widgets/place_time_picker.dart';
+import 'package:village/view/pages/place/detail_page/place_detail_page_view_model.dart';
 import 'package:village/view/pages/place/detail_page/widgets/place_date_picker.dart';
 import 'package:village/view/widgets/custom_bottom_button.dart';
 import 'package:village/view/widgets/button_icon.dart';
 
-class PlaceCustomBottomButton extends StatelessWidget {
+class PlaceCustomBottomButton extends ConsumerWidget {
   const PlaceCustomBottomButton({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    PlaceDetailPageModel pm = ref.read(placeDetailPageProvider);
+    PickerViewModel pickervm = ref.read(pickerProvider.notifier);
+    PickerModel? pickermodel = ref.watch(pickerProvider);
     // var controller = PrimaryScrollController.of(context);
+    DateTime startTime =
+        pickermodel?.startTime ?? DateTime(2016, 5, 10, 10, 35);
+    DateTime endTime = pickermodel?.endTime ?? DateTime(2016, 5, 10, 20, 45);
+    int maxPeopole = pickermodel?.maxPeople ?? 0;
+
     return Container(
       color: Colors.white,
       child: Padding(
@@ -52,13 +65,13 @@ class PlaceCustomBottomButton extends StatelessWidget {
                                   Container(
                                     margin: const EdgeInsets.only(bottom: 15),
                                     child: Row(
-                                      children: const [
-                                        Text(
+                                      children: [
+                                        const Text(
                                           '장소 선택  :     ',
                                           style: mblack_text,
                                         ),
                                         Text(
-                                          "장소 이름",
+                                          pm.place?.title ?? "",
                                           style: mblack_text,
                                         )
                                       ],
@@ -75,20 +88,34 @@ class PlaceCustomBottomButton extends StatelessWidget {
                                             left: 20,
                                           ),
                                           child: PlaceDatePicker()),
-                                      // Consumer(
-                                      //   builder: (context, ref, child) {
-                                      //     PlaceDetailPageModel pm = ref
-                                      //         .watch(placeDetailPageProvider);
-                                      //     return pm.resevasionDate != null
-                                      //         ? Text(pm.resevasionDate!)
-                                      //         : const Text('날짜 선택');
-                                      //   },
-                                      // )
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Consumer(
+                                        builder: (context, ref, child) {
+                                          PlaceDetailPageModel pm = ref
+                                              .watch(placeDetailPageProvider);
+                                          return pm.resevasionDate != null
+                                              ? Text(pm.resevasionDate!)
+                                              : const Text('<- 날짜 선택');
+                                        },
+                                      )
                                     ],
                                   ),
-                                  //  PlaceTimePicker(text: '예약 시작 시간'),
-                                  //  PlaceTimePicker(text: '예약 종료 시간'),
-                                  // PeoplePicker(text: '예약 인원'),
+                                  PlaceTimePicker(
+                                    funtion: pickervm.notifyChangeStartTime,
+                                    text: '예약 시작 시간',
+                                    dateTime: startTime,
+                                  ),
+                                  PlaceTimePicker(
+                                    funtion: pickervm.notifyChangeEndTime,
+                                    text: '예약 종료 시간',
+                                    dateTime: endTime,
+                                  ),
+                                  PeoplePicker(
+                                      funtion: pickervm.notifyChangeMaxPeople,
+                                      text: '예약 인원',
+                                      num: maxPeopole),
                                 ],
                               ),
                             ),
