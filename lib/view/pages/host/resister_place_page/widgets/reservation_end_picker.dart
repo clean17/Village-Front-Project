dc0cc98125ec69cd5e52bb05f8d486e0debfebe1
clean17 <piw940317@gmail.com>
2023-05-03 17:25:41 +0900

    예약 신청 폼

---
+++ lib/view/pages/host/resister_place_page/widgets/reservation_end_picker.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:village/core/constants/color.dart';
+import 'package:village/provider/picker_provier.dart';
+
+class ReservationEndPicker extends ConsumerWidget {
+  const ReservationEndPicker({
+    super.key,
+    required this.text,
+    required this.funtion,
+  });
+
+  final String text;
+  final funtion;
+
+  void _showDialog(Widget child, context) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 216,
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    PickerModel? pickermodel = ref.watch(pickerProvider);
+    DateTime endTime = pickermodel?.endTime ?? DateTime(2016, 5, 10, 10, 35);
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.white,
+      child: DefaultTextStyle(
+        style: TextStyle(
+          color: CupertinoColors.label.resolveFrom(context),
+          fontSize: 16.0,
+        ),
+        child: Row(
+          children: [
+            Text(text),
+            CupertinoButton(
+              onPressed: () => _showDialog(
+                CupertinoDatePicker(
+                  initialDateTime: endTime,
+                  mode: CupertinoDatePickerMode.time,
+                  use24hFormat: true,
+                  onDateTimeChanged: (DateTime newTime) {
+                    funtion(newTime);
+                  },
+                ),
+                context,
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: kPickColor,
+                ),
+                child: Text(
+                  '${endTime.hour}:${endTime.minute}',
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/pages/place/detail_page/widgets/place_botton_button.dart
+++ lib/view/pages/place/detail_page/widgets/place_botton_button.dart
@@ -4,8 +4,9 @@ import 'package:village/core/constants/move.dart';
 import 'package:village/core/constants/size.dart';
 import 'package:village/core/constants/style.dart';
 import 'package:village/provider/picker_provier.dart';
-import 'package:village/view/pages/host/resister_place_page/widgets/people_picker.dart';
-import 'package:village/view/pages/host/resister_place_page/widgets/place_time_picker.dart';
+import 'package:village/view/pages/host/resister_place_page/widgets/reservation_end_picker.dart';
+import 'package:village/view/pages/host/resister_place_page/widgets/reservation_people_picker.dart';
+import 'package:village/view/pages/host/resister_place_page/widgets/reservation_start_picker.dart';
 import 'package:village/view/pages/place/detail_page/place_detail_page_view_model.dart';
 import 'package:village/view/pages/place/detail_page/widgets/place_date_picker.dart';
 import 'package:village/view/widgets/custom_bottom_button.dart';
@@ -18,7 +19,7 @@ class PlaceCustomBottomButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    PlaceDetailPageModel pm = ref.read(placeDetailPageProvider);
+    PlaceDetailPageModel pm = ref.watch(placeDetailPageProvider);
     PickerViewModel pickervm = ref.read(pickerProvider.notifier);
     PickerModel? pickermodel = ref.watch(pickerProvider);
     // var controller = PrimaryScrollController.of(context);
@@ -26,7 +27,7 @@ class PlaceCustomBottomButton extends ConsumerWidget {
         pickermodel?.startTime ?? DateTime(2016, 5, 10, 10, 35);
     DateTime endTime = pickermodel?.endTime ?? DateTime(2016, 5, 10, 20, 45);
     int maxPeopole = pickermodel?.maxPeople ?? 0;
-
+    double leftPadding = getScreenWidth(context) * 0.13;
     return Container(
       color: Colors.white,
       child: Padding(
@@ -53,71 +54,66 @@ class PlaceCustomBottomButton extends ConsumerWidget {
                       return Stack(
                         children: [
                           Container(
-                            padding: const EdgeInsets.only(
-                                top: 20, bottom: 50, right: 20, left: 50),
-                            height: 350,
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.only(
+                                top: 20,
+                                bottom: 50,
+                                right: 20,
+                                left: leftPadding),
+                            height: 370,
                             color: Colors.white,
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Container(
-                                    margin: const EdgeInsets.only(bottom: 15),
-                                    child: Row(
-                                      children: [
-                                        const Text(
-                                          '장소 선택  :     ',
-                                          style: mblack_text,
-                                        ),
-                                        Text(
-                                          pm.place?.title ?? "",
-                                          style: mblack_text,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  Row(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 15),
+                                  child: Row(
                                     children: [
                                       const Text(
-                                        '날짜',
+                                        '장소 선택  :     ',
                                         style: mblack_text,
                                       ),
-                                      Container(
-                                          padding: const EdgeInsets.only(
-                                            left: 20,
-                                          ),
-                                          child: PlaceDatePicker()),
-                                      const SizedBox(
-                                        width: 20,
-                                      ),
-                                      Consumer(
-                                        builder: (context, ref, child) {
-                                          PlaceDetailPageModel pm = ref
-                                              .watch(placeDetailPageProvider);
-                                          return pm.resevasionDate != null
-                                              ? Text(pm.resevasionDate!)
-                                              : const Text('<- 날짜 선택');
-                                        },
+                                      Text(
+                                        pm.place?.title ?? "",
+                                        style: mblack_text,
                                       )
                                     ],
                                   ),
-                                  PlaceTimePicker(
-                                    funtion: pickervm.notifyChangeStartTime,
-                                    text: '예약 시작 시간',
-                                    dateTime: startTime,
-                                  ),
-                                  PlaceTimePicker(
-                                    funtion: pickervm.notifyChangeEndTime,
-                                    text: '예약 종료 시간',
-                                    dateTime: endTime,
-                                  ),
-                                  PeoplePicker(
-                                      funtion: pickervm.notifyChangeMaxPeople,
-                                      text: '예약 인원',
-                                      num: maxPeopole),
-                                ],
-                              ),
+                                ),
+                                Row(
+                                  children: [
+                                    const Text(
+                                      '날짜',
+                                      style: mblack_text,
+                                    ),
+                                    Container(
+                                        padding: const EdgeInsets.only(
+                                          left: 20,
+                                        ),
+                                        child: PlaceDatePicker()),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                  ],
+                                ),
+                                ReservationStartPicker(
+                                  funtion: pickervm.notifyChangeStartTime,
+                                  text: '예약 시작 시간',
+                                ),
+                                ReservationEndPicker(
+                                  funtion: pickervm.notifyChangeEndTime,
+                                  text: '예약 종료 시간',
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                ReservationPeoplePicker(
+                                  funtion: pickervm.notifyChangeMaxPeople,
+                                  text: '예약 인원',
+                                ),
+                              ],
                             ),
                           ),
                           Positioned(
@@ -127,7 +123,9 @@ class PlaceCustomBottomButton extends ConsumerWidget {
                               child: CustomBottomButton(
                                   mWeith: getScreenHeight(context),
                                   text: "예약신청",
-                                  funPress: null)),
+                                  funPress: () {
+                                    _showMyDialog(context, pickermodel);
+                                  })),
                           Positioned(
                               right: 20,
                               top: 20,
@@ -151,4 +149,40 @@ class PlaceCustomBottomButton extends ConsumerWidget {
       ),
     );
   }
+}
+
+Future<void> _showMyDialog(context, pickermodel) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('예약 신청을 하시겠습니까 ?'),
+        content: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListBody(
+              children: <Widget>[
+                Text('예약일 : ${pickermodel?.reservationDate}'),
+                Text(
+                    '예약시간 : ${pickermodel?.startTime} - ${pickermodel?.endTime}'),
+                Text('예약인원 : ${pickermodel?.maxPeople}'),
+                const Text('결제예정금액 : 40000'),
+              ],
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('아니오'),
+          ),
+          TextButton(
+            onPressed: () => {Navigator.pop(context, 'OK')},
+            child: const Text('예'),
+          ),
+        ],
+      );
+    },
+  );
 }
