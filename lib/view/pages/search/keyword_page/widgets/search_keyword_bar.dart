f709d546a3502c578da997c91f7203b26fbb0c86
LeeInHwa <2inhwa5@gmail.com>
2023-05-03 16:08:37 +0900

    검색기능,삭제기능 추가

--- lib/view/pages/search/keyword_page/widgets/search_keyword_bar.dart
+++ lib/view/pages/search/keyword_page/widgets/search_keyword_bar.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:village/core/constants/size.dart';
+import 'package:village/model/search/search.dart';
+import 'package:village/view/pages/search/keyword_page/search_keyword_page_view_model.dart';
+import 'package:village/view/pages/search/result_page/search_result_page.dart';
 
-class SearchKeywordBar extends StatelessWidget {
-  const SearchKeywordBar({
+class SearchKeywordBar extends ConsumerWidget{
+  const SearchKeywordBar( {
     required this.text,
     super.key,
     required TextEditingController searchController,
@@ -11,7 +16,7 @@ class SearchKeywordBar extends StatelessWidget {
   final TextEditingController _searchController;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       margin: const EdgeInsets.only(top: 1, left: 5),
       width: getScreenWidth(context) * 0.74,
@@ -20,6 +25,17 @@ class SearchKeywordBar extends StatelessWidget {
           color: Colors.grey[200], borderRadius: BorderRadius.circular(24)),
       child: TextField(
         controller: _searchController,
+        onSubmitted: (String keyword) async{
+          if (keyword.isNotEmpty) {
+            final viewModel = ref.read(searchKeywordPageProvider.notifier);
+            await viewModel.addSearch(Search(keyword: keyword));
+            Logger().d(keyword);
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => SearchResultPage(keyword: keyword)),
+            // );
+          }
+        },
         decoration: InputDecoration(
           prefixIcon: const Icon(Icons.search),
           hintText: text,
@@ -28,4 +44,5 @@ class SearchKeywordBar extends StatelessWidget {
       ),
     );
   }
+
 }
