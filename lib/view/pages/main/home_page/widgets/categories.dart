e99f86c4cf013fe8950b56d6f44eb98d4631f7f3
clean17 <piw940317@gmail.com>
2023-05-08 19:15:56 +0900

    카테고리 검색 완료

--- lib/view/pages/main/home_page/widgets/categories.dart
+++ lib/view/pages/main/home_page/widgets/categories.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:village/core/constants/move.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:village/controller/search_controller.dart';
 import 'package:village/view/pages/main/home_page/widgets/category_card.dart';
 
-class Categories extends StatelessWidget {
+class Categories extends ConsumerWidget {
   const Categories({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cp = ref.read(searchControllerProvider);
     List<Map<String, dynamic>> categories = [
       {"icon": "assets/icons/studying.png", "text": "스터디룸"},
       {"icon": "assets/icons/meeting.png", "text": "공유오피스"},
       {"icon": "assets/icons/studio.png", "text": "연습실"},
     ];
+    List<String> cate = ['스터디룸', '공유오피스', '연습실'];
     return Column(
       children: [
         const SizedBox(height: 30),
@@ -25,7 +28,7 @@ class Categories extends StatelessWidget {
                   icon: categories[index]["icon"],
                   text: categories[index]["text"],
                   press: () {
-                    Navigator.pushNamed(context, Move.placeCategoryPage);
+                    cp.searchCategory(cate[index]);
                   }),
             )
           ],
