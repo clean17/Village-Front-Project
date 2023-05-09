b20d6393d70bf2a49dfb1a8fc63ba6a17969470c
LeeInHwa <2inhwa5@gmail.com>
2023-05-09 12:21:31 +0900

    toast 수정

--- lib/view/pages/user/my_page/widgets/notification_option.dart
+++ lib/view/pages/user/my_page/widgets/notification_option.dart
@@ -2,10 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:village/core/constants/color.dart';
 import 'package:village/core/constants/style.dart';
+import 'package:village/view/widgets/my_error_motion_toast.dart';
+import 'package:village/view/widgets/my_success_motion_toast.dart';
 
 class NotificationOption extends StatefulWidget {
   final String title;
-
 
   NotificationOption({Key? key, required this.title}) : super(key: key);
 
@@ -14,9 +15,7 @@ class NotificationOption extends StatefulWidget {
 }
 
 class _NotificationOptionState extends State<NotificationOption> {
-
   bool valNotify = true;
-
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +32,19 @@ class _NotificationOptionState extends State<NotificationOption> {
             height: 20,
             width: 40,
             child: CupertinoSwitch(
-              activeColor: kPrimaryColor,
-              trackColor: Colors.grey,
-              value: valNotify,
-              onChanged: (bool newValue) {
-                setState(() {
-                  valNotify = newValue;
-                });
-              }
-            ),
+                activeColor: kPrimaryColor,
+                trackColor: Colors.grey,
+                value: valNotify,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    valNotify = newValue;
+                    if (valNotify) {
+                      MySuccessMotionToast("알림", "알림설정 완료");
+                    } else {
+                      MyErrorMotionToast("알림", "알림설정 해제");
+                    }
+                  });
+                }),
           ),
         ),
       ],
